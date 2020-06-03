b0d6c6a268ce87e0e30066f74293de902ebe4557
huangxing1023 <120331682@qq.com.cn>
2020-06-03 18:47:43 +0800

    2020-05-05：提交测试

--- bbking-web/src/main/java/com/family/bbkingweb/BbkingWebApplication.java
+++ bbking-web/src/main/java/com/family/bbkingweb/BbkingWebApplication.java
@@ -4,13 +4,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
 @SpringBootApplication(scanBasePackages = {"com.family.bbkingweb"})
-@MapperScan("com.family.bbkingweb.dao.mapper.*")
+@MapperScan("com.family.bbkingweb.dao.mapper")
 @EnableScheduling
 @EnableCaching
 public class BbkingWebApplication {
